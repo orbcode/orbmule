afb0c5b7ea6ba75ef2aa78e9ba678b72b840effe
Dave Marples <dave@marples.net>
2022-05-23 22:43:15 +0100

    Addition of simple demo for getting started with orbtrace

---
+++ examples/simple/inc/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <stdbool.h>
+
+// Interrupt Priorities ================
+// =====================================
+
+// Console configuration ===============
+// =====================================
+#define CONSOLE_SERIAL
+#define CONSOLE_SPEED       921600U
+#define CONSOLE_MAX_STR_LEN (128)
+
+// ===================================================================================
+// ==================== NO USER SERVICABLE PARTS BELOW THIS POINT ====================
+// ===================================================================================
+
+#endif /* CONFIG_H_ */
